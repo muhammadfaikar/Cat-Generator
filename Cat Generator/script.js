53a01aa003f48d8ed8b0b3af73228c72556137b9
Muhammad Faikar <muhammadfaikar@gmail.com>
2020-08-06 07:33:45 +0700

    Delete script.js

--- Cat Generator/script.js
+++
@@ -1,6 +0,0 @@
-function generateCat () {
-    const catGIF = document.createElement('img');
-    const catDiv = document.getElementById('catImages');
-    catGIF.src = "http://thecatapi.com/api/images/get?format=src&type=gif&size=small";
-    catDiv.appendChild (catGIF);
-}
